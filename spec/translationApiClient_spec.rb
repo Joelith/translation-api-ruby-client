1731e462d6026b5c69dd196b8ede02251f6bae8b
François Breard <francois.breard@systrangroup.com>
2016-02-18 11:56:04 -0800

    raise exception in case of a missing api key

--- spec/translationApiClient_spec.rb
+++ spec/translationApiClient_spec.rb
@@ -12,10 +12,10 @@ describe "TranslationApiClient" do
         if key.length > 0
           configuration.key = key
         else
-          puts "The key.txt file is empty"
+          raise "The apiKey.txt file is empty"
         end
       else
-        puts"The key.txt file doesn't exists"
+        raise "The apiKey.txt file doesn't exist"
       end
 
     end
